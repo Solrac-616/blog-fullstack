cb82f02ba5645c1036acfe0a1a8f5632f0821c94
Carlos Brito <carlos.e1998g@gmail.com>
2023-09-17 18:54:44 -0400

    get id

--- app/api/blog/route.ts
+++ app/api/blog/route.ts
@@ -1,7 +1,7 @@
 import prisma from "@/prisma";
 import { NextResponse } from "next/server";
 
-async function main() {
+export async function main() {
   try {
     await prisma.$connect();    
   } catch (error) {
@@ -11,16 +11,28 @@ async function main() {
 
 export const GET =async (req:Request, res:NextResponse) => {
   try {
-    console.log("GET");  
+    console.log("GET");
+    await main();
+    const posts = await prisma.post.findMany();
+    return NextResponse.json({ message: "Success", posts }, { status: 200 });
   } catch (error) {
-    return NextResponse.json({message:"ERRORGET", error}),{status:500}
+    return NextResponse.json({message:"ERRORGET", error},{status:500})
+  } finally {
+    await prisma.$disconnect();
   }
 }
 
-export const POST =async (req:Request, res:NextResponse) => {
+export const POST =async (req:Request, res:NextResponse) => 
+{
   try {
-    console.log("POST");  
+    console.log("POST");
+    const { title, description } = await req.json();
+    await main();
+    const post = await prisma.post.create({ data: { description, title } });
+    return NextResponse.json({ message: "Success CREATE POST", post }, { status: 201 });
   } catch (error) {
-    return NextResponse.json({message:"ERRORPOST", error})
+    return NextResponse.json({message:"ERRORPOST", error},{status:500})
+  } finally {
+    await prisma.$disconnect();
   }
 }
